efb363e3392cbb5d1273e0a62e9f3960aaf9bd25
user <user@DESKTOP-EVAU1DU>
2025-02-21 17:25:12 +0900

    2025/2/21

---
+++ baeljoon2/src/q2445/Main.java
@@ -0,0 +1,25 @@
+package q2445;
+
+import java.util.Scanner;
+
+public class Main {
+
+   public static void main(String[] args) {
+      Scanner sc = new Scanner(System.in);
+
+      int n = sc.nextInt();
+      sc.close();
+
+      for (int i = 1; i <= n; i++) {
+         String line = String.format("%s%s%s", "*".repeat(i), " ".repeat(2 * (n - i)), "*".repeat(i));
+         System.out.println(line);
+      }
+
+      for (int i = n - 1; i >= 1; i--) {
+         String line = String.format("%s%s%s", "*".repeat(i), " ".repeat(2 * (n - i)), "*".repeat(i));
+         System.out.println(line);
+      }
+   }
+   
+
+}
